bf56b5b97c23d67107cdf2ecb72d0fae78372ae6
nandabezerran <f.bezerra@alu.ufc.br>
2018-04-24 18:42:02 -0300

    RegularPow ERRO Codigo comentado OK

--- main.c
+++ main.c
@@ -9,7 +9,10 @@ int Columns    = 2;
 int Lines      = 2;
 int VectorSize = 2;
 
-
+/// Função para alocação da Matriz
+/// \param Lines = quantidade de linhas
+/// \param Columns = quantidade de colunas
+/// \return a matriz com o espaço alocado
 double** MatrixAlocation(int Lines, int Columns){
     double **Matrix = (double**)malloc(Lines * sizeof(double*));
 
@@ -20,6 +23,8 @@ double** MatrixAlocation(int Lines, int Columns){
     return Matrix;
 }
 
+/// Imprime a Matriz
+/// \param Matrix
 void PrintMatrix(double **Matrix){
     for (int l = 0; l < Lines; ++l) {
         for (int m = 0; m < Columns; ++m) {
@@ -30,67 +35,86 @@ void PrintMatrix(double **Matrix){
     }
 }
 
-double EuclidianNormalization(double *AutoVector) {
+/// Normalização do vetor
+/// \param Vector
+/// \return a norma do vetor
+double EuclidianNormalization(double *Vector) {
     double Normalization = 0;
     for (int j = 0; j < VectorSize; ++j) {
-        Normalization += pow(AutoVector[j], 2);
+        Normalization += pow(Vector[j], 2);
     }
     Normalization = sqrt(Normalization);
     return Normalization;
 }
 
-double* MatrixVectorMultiplication(double **Matrix, const double *AutoVector){
+/// Multiplica matriz e vetor
+/// \param Matrix
+/// \param Vector
+/// \return o vetor resultante da multiplicação
+double* MatrixVectorMultiplication(double **Matrix, const double *Vector){
     double *ResultingVector = (double*) malloc(VectorSize * sizeof(double));
     for (int i = 0; i < Lines ; ++i) {
         ResultingVector[i] = 0;
         for (int j = 0; j < Columns; ++j) {
-            ResultingVector[i] = AutoVector[j] * Matrix[i][j];
+            ResultingVector[i] += Vector[j] * Matrix[i][j];
         }
     }
     return ResultingVector;
 }
 
-double* CalculatingQ(double *vector) {
-    double *q = (double*) malloc(VectorSize * sizeof(double));
+/// Normaliza o vetor
+/// \param Vector
+/// \return o vetor normalizado
+double* NormalizingVector(double *Vector) {
+    double *NormalizedVector = (double*) malloc(VectorSize * sizeof(double));
     for (int i = 0; i < VectorSize; ++i) {
-        q[i] = vector[i]/EuclidianNormalization(vector);
+        NormalizedVector[i] = Vector[i]/EuclidianNormalization(Vector);
     }
+    return NormalizedVector;
 }
 
-double VectorTransposeMultiplication(const double *AutoVector1, const double *AutoVector2) {
+double VectorTransposeMultiplication(const double *Vector1, const double *Vector2) {
     double Result = 0;
     for (int i = 0; i < VectorSize ; ++i) {
-            Result += AutoVector1[i] * AutoVector2[i];
-
+        Result += Vector1[i] * Vector2[i];
     }
     return Result;
 }
 
-
-double* RegularPow(double **Matrix, double *InitialVector, double Tolerance){
-    double *q = CalculatingQ(InitialVector);
+/// Calcula o maior autovalor pelo metodo da potencia regular
+/// \param Matrix        = Matriz Original
+/// \param InitialVector = Vetor chute
+/// \param Tolerance     = Tolerancia para o erro
+/// \return o maior autovalor
+double RegularPow(double **Matrix, double *InitialVector, double Tolerance){
+    double *q = NormalizingVector(InitialVector);
     double Error;
     double Lambda;
     double PreviousLambda = 0;
     double *x = MatrixVectorMultiplication(Matrix, q);
 
     do{
-        q = CalculatingQ(x);
+        q = NormalizingVector(x);
         x = MatrixVectorMultiplication(Matrix, x);
         Lambda = VectorTransposeMultiplication(q,x);
         Error = fabs((Lambda - PreviousLambda)/Lambda);
     }
     while (Error > Tolerance );
-    return x;
+    return Lambda;
 }
 
+/// Função que faz a decomposição LU e encontra a inversa da matriz
+/// \param Matrix = Matriz original
+/// \return ponteiro para a inversa da Matriz original
 double** LUDecompositionForInverse(double **Matrix){
     double              **L = MatrixAlocation(Lines, Columns);
     double              **U = MatrixAlocation(Lines, Columns);
     double              **Y = MatrixAlocation(Lines, Columns);
     double **IdentityMatrix = MatrixAlocation(Lines, Columns);
-    double **InverseMatrix  = MatrixAlocation(Lines, Columns);
+    double  **InverseMatrix = MatrixAlocation(Lines, Columns);
 
+    //Decomposição LU -> Matriz = L * U
+    //Povoamento da Matriz identidade e das Matrizes L e U
     for (int l = 0; l < Lines ; ++l) {
         for (int m = 0; m < Columns; ++m) {
             L[l][m] = 0;
@@ -103,10 +127,8 @@ double** LUDecompositionForInverse(double **Matrix){
             }
         }
     }
-    printf("IdentityMatrix:\n");
-    PrintMatrix(IdentityMatrix);
-    printf("\n");
 
+    //Decomposição LU
     for(int i = 0; i < Lines; i++) {
         for(int j = 0; j < Columns; j++) {
             L[i][j] = IdentityMatrix[i][j];
@@ -128,13 +150,9 @@ double** LUDecompositionForInverse(double **Matrix){
             }
         }
     }
-    printf("L:\n");
-    PrintMatrix(L);
-    printf("\n");
-    printf("U:\n");
-    PrintMatrix(U);
-    printf("\n");
 
+    //Descobrindo o Y da formula para descobrir a Matriz inversa
+    // (Matriz * MatrizInversa = Identidade -> L * U * MatrizInversa = Identidade -> Y = U * MatrizInversa)
     for(int c = 0; c < Columns; c++) {
         for (int i = 0; i < Lines; ++i) {
             Y[i][c] = IdentityMatrix[i][c];
@@ -143,10 +161,8 @@ double** LUDecompositionForInverse(double **Matrix){
             }
         }
     }
-    printf("\nY:\n");
-    PrintMatrix(Y);
-    printf("\n");
 
+    //Descobrindo a MatrizInversa
     for(int c = 0; c < Columns; c++) {
         for (int i = Lines - 1; i >= 0; --i) {
             InverseMatrix[i][c] = Y[i][c];
@@ -159,22 +175,25 @@ double** LUDecompositionForInverse(double **Matrix){
     return InverseMatrix;
 }
 
-double* InversePow(double **Matrix, double *InitialVector, double Tolerance){
+double InversePow(double **Matrix, double *InitialVector, double Tolerance){
     double **InverseMatrix = LUDecompositionForInverse(Matrix);
     return RegularPow(InverseMatrix, InitialVector, Tolerance);
 }
 
 
 int main() {
-    double **Matrix = MatrixAlocation(Lines, Columns);
+    double       **Matrix = MatrixAlocation(Lines, Columns);
+    double *InitialVector = (double*)malloc(VectorSize * sizeof(double*));
+    double      Tolerance = 0.001;
+    InitialVector[0] = 1;
+    InitialVector[1] = 1;
     Matrix[0][0] = 1;
     Matrix[0][1] = 2;
     Matrix[1][0] = 3;
     Matrix[1][1] = 0;
-    double **InverseMatrix = LUDecompositionForInverse(Matrix);
-    PrintMatrix(InverseMatrix);
+    double AutoValue = RegularPow(Matrix, InitialVector, Tolerance);
+    printf("%f", AutoValue);
 
-    printf("Hello, World!\n");
     system("pause");
     return 0;
 }
